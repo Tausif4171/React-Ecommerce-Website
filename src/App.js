949cdb3aaecc4c23ace39472a0b59589aa901136
Tausif4171 <tausifkhan4171@gmail.com>
2020-03-15 16:02:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {Switch , Route} from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./Components/Navbar";
+import Productlist from "./Components/Productlist";
+import Details from "./Components/Details";
+import Cart from "./Components/Cart";
+import Default from "./Components/Default";
+import Modal from './Components/Modal';
+// import ProductProvider from './ProductProvider';
+
+class App extends Component{
+     render(){
+  return (
+    
+    <React.Fragment>
+       <Navbar />
+       <Switch>
+         
+         <Route  exact path="/" component={Productlist} />
+         <Route path="/details" component={Details}/>
+         <Route path="/cart" component={Cart}/>
+         <Route  component={Default} />
+       
+       </Switch>
+       <Modal />
+    </React.Fragment> 
+
+  );
+}
+}
+
+export default App;
